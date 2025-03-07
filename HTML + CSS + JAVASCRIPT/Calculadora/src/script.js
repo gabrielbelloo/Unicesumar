8c382091d38e9ec4103a3b7a24713d4a9116507e
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2025-03-07 15:25:33 -0300

    addValue update

--- HTML + CSS + JAVASCRIPT/Calculadora/src/script.js
+++ HTML + CSS + JAVASCRIPT/Calculadora/src/script.js
@@ -1,11 +1,23 @@
 function clearDisplay() {
-    document.getElementById("display").value = "";
+    document.getElementById("display").value = "0";
 }
 
 function addValue(value) {
-    document.getElementById("display").value += value;
+    if (document.getElementById("display").value >= "99999999999999999998"){
+        document.getElementById("display").value = "ERROR";
+
+        } else if (document.getElementById("display").value === "0") {
+        document.getElementById("display").value = value;
+
+            } else {
+            document.getElementById("display").value += value;
+            }
 }
 
 function calculate() {
         document.getElementById("display").value = eval(document.getElementById("display").value);
 }
+
+window.onload = function() {
+    clearDisplay();
+};
