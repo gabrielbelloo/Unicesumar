69b371429d1f977e31b95f255274a8a8f3683ed3
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2025-04-04 07:56:48 -0300

    Exercícios 18 à 26

--- HTML + CSS + JAVASCRIPT/60Exercicios/src/script.js
+++ HTML + CSS + JAVASCRIPT/60Exercicios/src/script.js
@@ -4,3 +4,18 @@ function enviarNomeEmail() {
     document.getElementById('mensagem').value = "";
     alert('Formulário enviado com sucesso!');
 }
+
+function olaMundoAlert() {
+    alert('Olá Mundo!');
+}
+
+function primeiroScriptConsole() {
+    console.log('Meu primeiro script em JavaScript');
+}
+
+function calculate(){
+    let num1 = 10;
+    let num2 = 20;
+    let resultado = num1 + num2;
+    console.log(num1 + ' + ' + num2 + ' = ' + resultado);
+}
