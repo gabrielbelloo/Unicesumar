000e13d1b3dbcba138a74c28a02642cfc2bf8952
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-03-24 10:09:01 -0300

    feat: add PedidoModel

---
+++ Java/lista-exercicios-marco/pedido/src/main/java/com/bello/pedido/models/PedidoModel.java
@@ -0,0 +1,53 @@
+package com.bello.pedido.models;
+
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_pedido")
+public class PedidoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "data_pedido")
+    private LocalDateTime data;
+    @Column(name = "valorTotal_pedido")
+    private BigDecimal valorTotal;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
