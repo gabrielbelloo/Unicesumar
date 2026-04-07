df07d460e2b3d8cb5b49b323403d6ee6fc7cf9df
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-04-07 09:02:20 -0300

    feat: add RevisaoRepository

---
+++ Java/revisao-01bim2026/src/main/java/com/bello/revisao_01bim2026/repositories/RevisaoRepository.java
@@ -0,0 +1,7 @@
+package com.bello.revisao_01bim2026.repositories;
+
+import com.bello.revisao_01bim2026.models.RevisaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RevisaoRepository extends JpaRepository<RevisaoModel, Long> {
+}
