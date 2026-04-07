a877494cae1bad4995d594a8a74eb4e05b6ce8f8
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-04-07 13:41:55 -0300

    feat: add ProdutoService

---
+++ Java/1Sem2026/Revisão 1/produto/src/main/java/com/bello/produto/services/ProdutoService.java
@@ -0,0 +1,41 @@
+package com.bello.produto.services;
+
+import com.bello.produto.models.ProdutoModel;
+import com.bello.produto.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    public ProdutoModel add(ProdutoModel produtoModel){
+        return produtoRepository.save(produtoModel);
+    }
+
+    public List<ProdutoModel> listar(){
+        return produtoRepository.findAll();
+    }
+
+    public Optional<ProdutoModel> buscar(Long id){
+        return produtoRepository.findById(id);
+    }
+
+    public void deletar(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+    public ProdutoModel atualizar(Long id, ProdutoModel produtoModel){
+        ProdutoModel model = produtoRepository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+
+        model.setNome(produtoModel.getNome());
+        model.setPreco(produtoModel.getPreco());
+
+        return produtoRepository.save(model);
+    }
+}
