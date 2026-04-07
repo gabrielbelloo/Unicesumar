a30e586ffef02833fdd61fc1c50e78efec715f26
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-04-07 15:28:44 -0300

    feat: add PedidoRepository

---
+++ Java/1Sem2026/Revisão 1/pedido/src/main/java/com/bello/pedido/repositories/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.bello.pedido.repositories;
+
+import com.bello.pedido.models.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+}
