9f9194caf271a9de6116ab0df0c26a107c2e47ee
Gabriel Bello <167886489+gabrielbelloo@users.noreply.github.com>
2026-05-26 22:12:04 -0300

    feat(): add spring security to produto

---
+++ Java/lista-exercicios-marco/produto/src/main/java/com/bello/produto/filters/SecurityFilter.java
@@ -0,0 +1,24 @@
+package com.bello.produto.filters;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/produtos").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/produtos").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/pedidos/**").permitAll())
+                .build();
+    }
+}
