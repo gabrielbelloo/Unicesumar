9887fe293fdedd3e1dcc351c25b6c2be851d8854
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-06-15 08:25:00 -0300

    feat: add ProdutoModel

---
+++ Java/revisao-02bim2026/produtos/src/main/java/com/bello/produtos/models/ProdutoModel.java
@@ -0,0 +1,15 @@
+package com.bello.produtos.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "TBL_PRODUTOS")
+@Data
+public class ProdutoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
